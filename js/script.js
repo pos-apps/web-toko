fda680a09b940fe1fe5368daef9a6c780ee59954
Danis Zaidan <daniszaidan@gmail.com>
2019-10-05 14:28:22 +0700

    sidebar navigation

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $("#sidebar").mCustomScrollbar({
+        theme: "minimal"
+    });
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar, #content').toggleClass('active');
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+
+    // $('#dismiss, .overlay').on('click', function () {
+    //     $('#sidebar').removeClass('active');
+    //     $('.overlay').removeClass('active');
+    // });
+
+    // $('#sidebarCollapse').on('click', function () {
+    //     $('#sidebar').addClass('active');
+    //     $('.overlay').addClass('active');
+    //     $('.collapse.in').toggleClass('in');
+    //     $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    // });
+});
