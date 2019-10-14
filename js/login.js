124bfebff35dca09e4b79c8bd680069c95af5774
bacharuddinadieb <bacharuddinadiebpratama@gmail.com>
2019-10-14 10:00:40 +0700

    Fungsi login dan penambahan kunci OAuth2 ke locsto

--- js/login.js
+++ js/login.js
@@ -9,13 +9,38 @@
                 if (form.checkValidity() === false) {
                     event.preventDefault();
                     event.stopPropagation();
+                } else if (form.checkValidity() === true) {
+                    event.preventDefault();
+                    event.stopPropagation();
+
+                    let username = document.getElementById('input-username').value;
+                    let password = document.getElementById('input-password').value;
+
+                    axios.post('http://localhost:3000/authentication/login', {
+                        username: username,
+                        password: password
+                    }).then(function (response) {
+                        console.log(response.data);
+                        if (response.data.status == "Error") {
+                            Swal.fire({
+                                type: 'error',
+                                title: 'Gagal Login...',
+                                text: 'Username atau Password salah...'
+                            })
+                        } else if (response.data.Status == "Login berhasil!") {
+                            console.log(response.data.data);
+                            if (typeof (Storage) !== undefined) {
+                                window.localStorage.setItem('OAuth2_PassTok', JSON.stringify(response.data.data));
+                            } else {
+                                console.log('Web Browser tidak support local storage');
+                            }
+                        }
+                    }).catch(function (err) {
+                        console.log(err);
+                    })
                 }
                 form.classList.add('was-validated');
             }, false);
         });
     }, false);
 })();
-
-document.querySelector("#login-form").addEventListener("submit", function () {
-
-})
